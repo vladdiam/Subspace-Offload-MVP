b4a84d804fcbd7f8ebf4abd896134662271caa10
Vlad Boretskyi <vladboretskyi.work@gmail.com>
2026-03-20 23:33:20 +0200

    2nd step - Add URL replacing logic & related

--- subspace-offload.php
+++ subspace-offload.php
@@ -8,7 +8,7 @@
 
 if ( ! defined( 'ABSPATH' ) ) exit; //prevent direct entry via url
 
-add_action( 'admin_menu', 'sso_add_admin_menu' );
+add_action( 'admin_menu', 'sso_add_admin_menu' ); // ============== MODULE 1 : fields in admin & related ===
 function sso_add_admin_menu() {
     add_options_page(
         'Subspace Offload', //tab title
@@ -53,3 +53,31 @@ function sso_settings_init() { //registers options names \/
         echo '<input type="month" name="sso_cutoff_date" value="' . esc_attr($val) . '">';
     }, 'subspace-offload', 'sso_main_section' );
 }
+
+//=============== MODULE 2 : URL filtering & logic ===
+add_filter( 'wp_get_attachment_url', 'sso_maybe_offload_url', 10, 2 );//target attachment url hook, set priority to 10(def) and indicate 2 args
+
+function sso_maybe_offload_url( $url, $post_id ) {
+    $cdn_url = get_option('sso_cdn_url');//get value from cdn-url field in admin & assign to this var
+    $cutoff_date = get_option('sso_cutoff_date');//cutoff date field
+
+    if ( empty($cdn_url) || empty($cutoff_date) ) {//if smth is empty, return original url
+        return $url;
+    }
+
+    $post = get_post($post_id);//retrieve the object of this post (attachment)
+    if ( ! $post ) return $url;//returng og. url if is not found
+    
+    $file_date = date( 'Y-m', strtotime( $post->post_date ) );//get post date & cut it to 2026-03 format 
+
+    if ( $file_date <= $cutoff_date ) {//if file date is older or equal to date in admin field, then \/
+        $upload_dir = wp_upload_dir();//get /uploads folder url
+        $base_url = $upload_dir['baseurl'];//get url of upload dir from upload_dir array
+
+        $url = str_replace( $base_url, untrailingslashit($cdn_url), $url );//replace url & erase the slash
+    }
+
+    return $url;//returns new value
+}
+
+// MODULE IS WORKING. URL ARE REPLACING. BUT IMG'S ARE DISPLAYING ON SITE (HARDCODE, DIFF. SIZES)
